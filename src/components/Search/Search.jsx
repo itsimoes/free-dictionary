26e9d0dd850463b8f877a5ab9e170757e64ed9e1
Italo Simões <2d.italo@gmail.com>
2023-07-28 10:12:21 -0300

    Bloco 'meanings' adicionado e estilizado'

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -9,11 +9,13 @@ import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 const Search = () => {
   const [searchField, setSearchField] = useState("");
+  const [data, setData] = useState("");
   const [word, setWord] = useState("");
   const [phonetic, setPhonetic] = useState("");
-  const [audio, setAudio] = useState("");
+  const [audioLink, setAudioLink] = useState("");
   const [source, setSource] = useState("");
-  const audio2 = new Audio("");
+  const [meanings, setMeanings] = useState("");
+  const audio = new Audio("");
 
   function dictionaryResearch() {
     console.log("success!"); //teste
@@ -21,47 +23,51 @@ const Search = () => {
     const wordWrapper = document.getElementById("word-wrapper");
     const playButton = document.getElementById("play-button");
     const sourceLink = document.getElementById("source");
-    
-    if (searchField !== "") {
-      wordWrapper.style.display = "flex";
-      sourceLink.style.display = "flex";
-    } else {
-      wordWrapper.style.display = "none";
-      sourceLink.style.display = "none";
-    }
+
     axios
       .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchField}`)
       // console.log(searchField);
       .then(function (result) {
-        const data = result.data;
-
-        setWord(data);
+        // const data = result.data;
+        console.log(result);
+        setData((prevData) => result.data);
         setWord(result.data[0].word);
         setPhonetic(result.data[0].phonetics[1].text);
-        setAudio(result.data[0].phonetics[0].audio);
+        setAudioLink(result.data[0].phonetics[0].audio);
         setSource(result.data[0].sourceUrls);
+        setMeanings(result.data[0].meanings);
 
-        audio2.src = result.data[0].phonetics[0].audio;
+        if (searchField !== "") {
+          wordWrapper.style.display = "flex";
+          sourceLink.style.display = "flex";
+        } else {
+          wordWrapper.style.display = "none";
+          sourceLink.style.display = "none";
+        }
+
+        audio.src = result.data[0].phonetics[0].audio;
+
         if (result.data[0].phonetics[0].audio !== "") {
           playButton.style.display = "flex";
         } else {
           playButton.style.display = "none";
         }
 
-        // if (result.data[0].sourceUrls !== "") {
-        //   sourceLink.style.display = "flex";
-        // } else {
-        //   sourceLink.style.display = "none";
-        // }
-
         //testes
         console.log(data);
         console.log(word);
+        console.log(phonetic);
+        console.log(audioLink);
         console.log(audio);
-        console.log(audio2);
         console.log(source);
+        console.log(meanings);
+        console.log(result.data[0].meanings.length);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        wordWrapper.style.display = "none";
+        sourceLink.style.display = "none";
+        console.log(err);
+      });
   }
 
   const handleChange = (e) => {
@@ -71,10 +77,43 @@ const Search = () => {
   };
 
   const start = () => {
-    audio2.setAttribute("src", audio);
-    audio2.load();
-    audio2.play();
+    audio.setAttribute("src", audioLink);
+    audio.load();
+    audio.play();
   };
+
+  const meaningBlock = () => {
+    var resp = [];
+    var definitions = [];
+
+    for (var i = 0; i < meanings.length; i++) {
+      for (var j = 0; j < meanings[i].definitions.length; j++) {
+        definitions[j] = (
+          <li key={meanings[i].definitions[j].definition}>
+            <div className="li-bullet">•</div>
+            <p>{meanings[i].definitions[j].definition}</p>
+          </li>
+        );
+      }
+      console.log(meanings[i].definitions);     
+
+      resp[i] = (
+        <>
+          <h3 className="definitions" key={meanings[i].partOfSpeech}>
+            <div>{meanings[i].partOfSpeech}</div>
+            <hr className="divider" />
+          </h3>
+          <h4>Meanings</h4>
+          <ul key={meanings[i].definitions}>{definitions}</ul>
+        </>
+      );
+      definitions = [];
+    }
+    console.log(resp);
+    return resp;
+  };
+
+  var meaningBlocks = meaningBlock();
 
   return (
     <>
@@ -96,25 +135,31 @@ const Search = () => {
         </div>
       </form>
       <div id="word-wrapper">
-        <div className="word-result-wrapper">
-          <div id="word">{word}</div>
-          <div id="phonetic">{phonetic}</div>
+        <div className="word-title-wrapper">
+          <div className="word-result-wrapper">
+            <div id="word">{word}</div>
+            <div id="phonetic">{phonetic}</div>
+          </div>
+          <div className="audio-result-wrapper">
+            <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+              <IconButton
+                onClick={start}
+                aria-label="play/pause"
+                id="play-button"
+              >
+                <PlayArrowIcon
+                  sx={{ height: 38, width: 38, color: "blueviolet" }}
+                />
+              </IconButton>
+            </Box>
+          </div>
         </div>
-        <div className="audio-result-wrapper">
-          <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-            <IconButton
-              onClick={start}
-              aria-label="play/pause"
-              id="play-button"
-            >
-              <PlayArrowIcon
-                sx={{ height: 38, width: 38, color: "blueviolet" }}
-              />
-            </IconButton>
-          </Box>
+        <div>
+          <div id="meanings">{meaningBlocks}</div>
         </div>
+        <hr id="divider-2" />
+        <h5 id="source">Source <a href={source} target="_blank" rel="noreferrer"> {source}</a></h5>
       </div>
-      <div id="source">Source {source}</div>
     </>
   );
 };
